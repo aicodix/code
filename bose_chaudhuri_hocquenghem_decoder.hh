6b3bc5e534d2c1d3b4f8bdfae782beec6597c22d
Ahmet Inan <inan@aicodix.de>
2018-09-27 10:33:47 +0200

    added update_syndromes() helper

--- bose_chaudhuri_hocquenghem_decoder.hh
+++ bose_chaudhuri_hocquenghem_decoder.hh
@@ -20,21 +20,27 @@ public:
 	typedef typename GF::ValueType ValueType;
 	typedef typename GF::IndexType IndexType;
 	static const int N = GF::N, K = MSG, NP = N - K;
+private:
 	ReedSolomonErrorCorrection<NR, FCR, GF> algorithm;
-	int compute_syndromes(uint8_t *code, ValueType *syndromes)
+	void update_syndromes(uint8_t *poly, ValueType *syndromes, int begin, int end)
 	{
-		// $syndromes_i = code(pe^{FCR+i})$
-		ValueType coeff(get_be_bit(code, 0));
-		for (int i = 0; i < NR; ++i)
-			syndromes[i] = coeff;
-		for (int j = 1; j < N; ++j) {
-			ValueType coeff(get_be_bit(code, j));
+		for (int j = begin; j < end; ++j) {
+			ValueType coeff(get_be_bit(poly, j));
 			IndexType root(FCR), pe(1);
 			for (int i = 0; i < NR; ++i) {
 				syndromes[i] = fma(root, syndromes[i], coeff);
 				root *= pe;
 			}
 		}
+	}
+public:
+	int compute_syndromes(uint8_t *code, ValueType *syndromes)
+	{
+		// $syndromes_i = code(pe^{FCR+i})$
+		ValueType coeff(get_be_bit(code, 0));
+		for (int i = 0; i < NR; ++i)
+			syndromes[i] = coeff;
+		update_syndromes(code, syndromes, 1, N);
 		int nonzero = 0;
 		for (int i = 0; i < NR; ++i)
 			nonzero += !!syndromes[i];
@@ -79,21 +85,27 @@ public:
 	typedef typename GF::ValueType ValueType;
 	typedef typename GF::IndexType IndexType;
 	static const int N = GF::N, K = MSG, NP = N - K;
+private:
 	ReedSolomonErrorCorrection<NR, FCR, GF> algorithm;
-	int compute_syndromes(ValueType *code, ValueType *syndromes)
+	void update_syndromes(ValueType *poly, ValueType *syndromes, int begin, int end)
 	{
-		// $syndromes_i = code(pe^{FCR+i})$
-		ValueType coeff(code[0]);
-		for (int i = 0; i < NR; ++i)
-			syndromes[i] = coeff;
-		for (int j = 1; j < N; ++j) {
-			ValueType coeff(code[j]);
+		for (int j = begin; j < end; ++j) {
+			ValueType coeff(poly[j]);
 			IndexType root(FCR), pe(1);
 			for (int i = 0; i < NR; ++i) {
 				syndromes[i] = fma(root, syndromes[i], coeff);
 				root *= pe;
 			}
 		}
+	}
+public:
+	int compute_syndromes(ValueType *code, ValueType *syndromes)
+	{
+		// $syndromes_i = code(pe^{FCR+i})$
+		ValueType coeff(code[0]);
+		for (int i = 0; i < NR; ++i)
+			syndromes[i] = coeff;
+		update_syndromes(code, syndromes, 1, N);
 		int nonzero = 0;
 		for (int i = 0; i < NR; ++i)
 			nonzero += !!syndromes[i];
--- reed_solomon_decoder.hh
+++ reed_solomon_decoder.hh
@@ -19,21 +19,27 @@ public:
 	typedef typename GF::ValueType ValueType;
 	typedef typename GF::IndexType IndexType;
 	static const int N = GF::N, K = N - NR, NP = NR;
+private:
 	ReedSolomonErrorCorrection<NR, FCR, GF> algorithm;
-	int compute_syndromes(ValueType *code, ValueType *syndromes)
+	void update_syndromes(ValueType *poly, ValueType *syndromes, int begin, int end)
 	{
-		// $syndromes_i = code(pe^{FCR+i})$
-		ValueType coeff(code[0]);
-		for (int i = 0; i < NR; ++i)
-			syndromes[i] = coeff;
-		for (int j = 1; j < N; ++j) {
-			ValueType coeff(code[j]);
+		for (int j = begin; j < end; ++j) {
+			ValueType coeff(poly[j]);
 			IndexType root(FCR), pe(1);
 			for (int i = 0; i < NR; ++i) {
 				syndromes[i] = fma(root, syndromes[i], coeff);
 				root *= pe;
 			}
 		}
+	}
+public:
+	int compute_syndromes(ValueType *code, ValueType *syndromes)
+	{
+		// $syndromes_i = code(pe^{FCR+i})$
+		ValueType coeff(code[0]);
+		for (int i = 0; i < NR; ++i)
+			syndromes[i] = coeff;
+		update_syndromes(code, syndromes, 1, N);
 		int nonzero = 0;
 		for (int i = 0; i < NR; ++i)
 			nonzero += !!syndromes[i];
